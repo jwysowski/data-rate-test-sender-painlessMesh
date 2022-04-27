a21774258bef316868a3edb9c4d012a54936354b
jakub_wysowski <jakub.wysowski@geotronics.com.pl>
2022-04-27 10:04:25 +0200

    Sketch of data rate test sending program. Require checking

---
+++ include/data.hpp
@@ -0,0 +1,17 @@
+#ifndef TRENDS_HPP
+#define TRENDS_HPP
+
+#define BAUDRATE        9600
+
+//DHT
+#define DHTPIN          	2
+#define DHTTYPE         	DHT11
+#define OVERFLOW_LIMIT  	0x7FFFFF
+
+//mesh
+#define PORT            5555
+
+extern const char *ssid;
+extern const char *password;
+    
+#endif /* TRENDS_HPP */
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,51 @@
 #include <Arduino.h>
+#include <painlessMesh.h>
+#include <string.h>
 
+#include "data.hpp"
+
+#define MESSAGE_COUNTER_SIZE    3
+#define OFFSET_SIZE             10
+
+void received_callback(const uint32_t &from, const String &msg);
+
+char count[MESSAGE_COUNTER_SIZE + 1];
+painlessMesh mesh;
+uint16_t message_counter = 0;
+uint32_t prev_millis = 0;
+uint32_t interval_ms = 1000;
+String message_offset;
 void setup() {
-  // put your setup code here, to run once:
+	Serial.begin(BAUDRATE);
+
+	//mesh
+	// mesh.setDebugMsgTypes(ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE);   // all types on
+
+	mesh.init(ssid, password, PORT);
+	mesh.setContainsRoot(true);
+	mesh.onReceive(&received_callback);
+
+    //message_offset
+    for (int i = 0; i < OFFSET_SIZE; i++)
+        message_offset += String(':');
+
+	//dht
+	timer1_attachInterrupt(timer_overflow);
+	timer1_enable(TIM_DIV16, TIM_EDGE, TIM_SINGLE);         //5MHz
+	timer1_write(OVERFLOW_LIMIT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	mesh.update();
+    uint32_t curr_millis = millis();
+	if (curr_millis - prev_millis > interval_ms) {
+        prev_millis = curr_millis;
+        
+        sprintf(count, "%03x", message_counter);
+		mesh.sendBroadcast(String(ESP.getChipId()) + String('\t') + String(count) + message_offset);
+        message_counter++;
+	}
+}
+
+void received_callback(const uint32_t &from, const String &msg) {
 }
